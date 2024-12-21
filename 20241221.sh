a2bdb5d18b5a2fefe513cce64a45db8f7c3a47c7
FeiWang1998 <105405712+FeiWang1998@users.noreply.github.com>
2024-12-21 16:37:46 +0800

    Create 20241221.sh

---
+++ 20241221.sh
@@ -0,0 +1,17 @@
+apt update
+apt install unzip -y
+
+
+fnm use --install-if-missing 22
+
+
+node -v # should print v22.12.0
+
+
+npm -v # should print 10.9.0
+
+npm install -g npm@11.0.0
+
+npm i -g rivalz-node-cli
+
+screen -S znoderivalz

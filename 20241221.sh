d60ae22b489030f634a80f00a218f4cb49946ed8
FeiWang1998 <105405712+FeiWang1998@users.noreply.github.com>
2024-12-21 21:40:10 +0800

    Update 20241221.sh

--- 20241221.sh
+++ 20241221.sh
@@ -1,5 +1,27 @@
-git clone https://github.com/zphgzpkf/New-repository.git
-cd New-repository
-chmod +x 20241220.sh
-./20241220.sh
+apt update
+apt install unzip -y
+
+# download and install Node.js
+fnm use --install-if-missing 22
+
+
+
+npm install -g npm@11.0.0
+
+npm i -g rivalz-node-cli
+
+apt install screen  
+
+# installs fnm (Fast Node Manager)
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# activate fnm
+source ~/.bashrc
+
+
+
+
+
+screen -S znoderivalz
+
 rivalz run

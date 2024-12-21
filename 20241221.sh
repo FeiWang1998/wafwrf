dac314e4818676b722de00a4076de52f5ce287fc
FeiWang1998 <105405712+FeiWang1998@users.noreply.github.com>
2024-12-21 21:49:21 +0800

    Update 20241221.sh

--- 20241221.sh
+++ 20241221.sh
@@ -20,6 +20,26 @@ source ~/.bashrc
 
 
 
+screen -S znoderivalz
+
+rivalz run
+
+# download and install Node.js
+fnm use --install-if-missing 22
+
+
+
+npm install -g npm@11.0.0
+
+npm i -g rivalz-node-cli
+
+
+# installs fnm (Fast Node Manager)
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# activate fnm
+source ~/.bashrc
+
 
 
 screen -S znoderivalz

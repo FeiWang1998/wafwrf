0008011115b05dd467093cfb45e92349debc33a5
FeiWang1998 <105405712+FeiWang1998@users.noreply.github.com>
2024-12-21 21:46:15 +0800

    Update 20241221.sh

--- 20241221.sh
+++ 20241221.sh
@@ -1,5 +1,6 @@
 apt update
 apt install unzip -y
+apt install screen  
 
 # download and install Node.js
 fnm use --install-if-missing 22
@@ -10,7 +11,6 @@ npm install -g npm@11.0.0
 
 npm i -g rivalz-node-cli
 
-apt install screen  
 
 # installs fnm (Fast Node Manager)
 curl -fsSL https://fnm.vercel.app/install | bash
